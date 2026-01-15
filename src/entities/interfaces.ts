611b2344bcec716330b92b82ea49cefa892e9209
arifaeva <arifa.eva.ixa@gmail.com>
2026-01-15 20:25:57 +0700

    feat(interfaces): add IAuthRepository and extend IUserRepository

--- src/entities/interfaces.ts
+++ src/entities/interfaces.ts
@@ -1,8 +1,18 @@
-import { User } from "../generated/prisma/client";
+import { User, LoginSession } from "../generated/prisma/client";
+
+export interface IAuthRepository {
+  register(name: string, email: string, password: string): Promise<User | null>;
+  login(email: string, password: string): Promise<LoginSession | null>;
+}
 
 export interface IUserRepository {
   findAllUsers(): Promise<User[] | null>;
   findUserById(id: string): Promise<User | null>;
   findUserByEmail(email: string): Promise<User | null>;
-  createUser(email: string, password: string): Promise<User | null>;
+  createUser(
+    name: string,
+    email: string,
+    password: string
+  ): Promise<User | null>;
+  createStudent(email: string): Promise<User | null>;
 }
