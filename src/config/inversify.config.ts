3811d935ea8395d4dcd0c920d29ccccfcc98ad16
arifaeva <arifa.eva.ixa@gmail.com>
2026-01-18 23:22:59 +0700

    fix: correct inversify bindings

--- src/config/inversify.config.ts
+++ src/config/inversify.config.ts
@@ -1,16 +1,16 @@
 import { Container } from "inversify";
+import { TYPES } from "../entities/types";
 
 import { UserServices } from "../services/userServices";
 import { AuthServices } from "../services/authServices";
 
 import { UserRepo } from "../repositories/userRepo";
 import { AuthRepo } from "../repositories/authRepo";
-import { TYPES } from "../entities/types";
 
 const container = new Container();
 
-container.bind<UserRepo>(TYPES.AuthRepo).to(UserRepo);
-container.bind<AuthRepo>(TYPES.UserRepo).to(AuthRepo);
+container.bind<UserRepo>(TYPES.UserRepo).to(UserRepo);
+container.bind<AuthRepo>(TYPES.AuthRepo).to(AuthRepo);
 
 container.bind<UserServices>(UserServices).toSelf();
 container.bind<AuthServices>(AuthServices).toSelf();
