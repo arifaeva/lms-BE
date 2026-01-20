1ce5cc2da374e757d99ab5eb3f51c96f68b96aaf
arifaeva <arifa.eva.ixa@gmail.com>
2026-01-20 22:11:48 +0700

    chore: enable CORS for development

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Elysia } from "elysia";
 import { authRouter } from "./router/auth";
 import { userRouter } from "./router/user";
+import { cors } from "@elysiajs/cors";
 
 const app = new Elysia()
-
+  .use(cors())
+  .get("/", () => "Hello this is LMS backend API")
   .group("api/v1", (app) => app.use(authRouter).use(userRouter))
   .listen(3000);
 
