dcb925ea8ddad7c7f6797a253b9fbc46d0355eca
arifaeva <arifa.eva.ixa@gmail.com>
2026-02-04 23:38:04 +0700

    feat: add set cookie in login endpoint and add logout endpoint

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -14,14 +14,27 @@ export const authRouter = new Elysia({ prefix: "/auth" })
         email: t.String({ format: "email" }),
         password: t.String(),
       }),
-    }
+    },
   )
 
   .post(
     "/login",
-    async ({ body }) => {
+    async ({ body, cookie: { session_id }, set }) => {
       const { email, password } = body;
       const session = await authService.login(email, password);
+
+      if (!session) {
+        set.status = 401;
+        return { message: "Invalid email or password" };
+      }
+
+      session_id.set({
+        value: session.sessionId,
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax",
+        path: "/",
+      });
 
       return {
         userId: session.userId,
@@ -34,5 +47,11 @@ export const authRouter = new Elysia({ prefix: "/auth" })
         email: t.String(),
         password: t.String(),
       }),
-    }
-  );
+    },
+  )
+
+  .post("/logout", async ({ cookie: { session_id } }) => {
+    session_id.remove();
+
+    return { message: "Logged out" };
+  });
